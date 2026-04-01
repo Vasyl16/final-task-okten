bb65fcf15a530066c0f85525e2770a2034cc68bf
Yurchuk Vasyl <mira07081984@gmail.com>
2026-04-01 15:39:38 +0300

    fix docker-compose file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,12 @@
-import { AppProviders } from '@/app/providers/app-providers'
-import { AppRouterProvider } from '@/app/router/router-provider'
+import { AppProviders } from '@/app/providers/app-providers';
+import { AppRouterProvider } from '@/app/router/router-provider';
 
 function App() {
   return (
     <AppProviders>
       <AppRouterProvider />
     </AppProviders>
-  )
+  );
 }
 
-export default App
+export default App;
